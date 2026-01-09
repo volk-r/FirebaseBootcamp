19da3d368a0bee255e163e7079aa66dbe28b4271
Roman Romanov <rromanov.vk@gmail.com>
2026-01-09 15:47:03 +0500

    connect Firebase to project

--- FirebaseBootcamp/FirebaseBootcampApp.swift
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseBootcampApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	// register app delegate for Firebase setup
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+	) -> Bool {
+		FirebaseApp.configure()
+
+		return true
+	}
 }
