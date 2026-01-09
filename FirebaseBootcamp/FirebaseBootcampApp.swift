8fe4ebc954be1492db645fffd15c3c9634eca82e
Roman Romanov <rromanov.vk@gmail.com>
2026-01-09 14:59:16 +0500

    Initial commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Roman Romanov on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
